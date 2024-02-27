64e3aaa5aa7e691737754f0e3f632af107491f75
maros <kocur.maros@gmail.com>
2024-02-27 16:01:34 +0100

    fx fy cx cy for all pics

--- calibration.py
+++ calibration.py
@@ -46,8 +46,8 @@ if use_ximea == True:
 
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((5*6,3), np.float32)
-objp[:,:2] = np.mgrid[0:6,0:5].T.reshape(-1,2)
+objp = np.zeros((5*7,3), np.float32)
+objp[:,:2] = np.mgrid[0:7,0:5].T.reshape(-1,2)
 
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
@@ -63,14 +63,14 @@ for fname in images:
     # Find the chess board corners
     cv.imshow('img', gray)
     cv.waitKey()
-    ret, corners = cv.findChessboardCorners(gray, (6,5), None)
+    ret, corners = cv.findChessboardCorners(gray, (7,5), None)
     # If found, add object points, image points (after refining them)
     if ret == True:
         objpoints.append(objp)
         corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
         imgpoints.append(corners2)
         # Draw and display the corners
-        cv.drawChessboardCorners(img, (6,5), corners2, ret)
+        cv.drawChessboardCorners(img, (7,5), corners2, ret)
         cv.imshow('img', img)
         _, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
         h,  w = img.shape[:2]
