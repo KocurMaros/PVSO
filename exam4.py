f42456bf2401a9cce7bca8781dde26aed6eebbea
KocurMaros <kocur.maros@gmail.com>
2024-04-14 21:00:48 +0200

    select points with ransac

--- exam4.py
+++ exam4.py
@@ -61,11 +61,25 @@ if __name__ == "__main__":
 
     # Visualize the point cloud
     # o3d.visualization.draw_geometries([pcd])
+    plane_model, inliers = pcd.segment_plane(distance_threshold=0.1,  # Max distance a point can be from the plane model, and still be considered an inlier.
+                                            ransac_n=3, #Number of initial points to be considered inliers in each iteration.
+                                            num_iterations=5000)
+    [a, b, c, d] = plane_model
+    print(f"Plane equation: {a:.2f}x + {b:.2f}y + {c:.2f}z + {d:.2f} = 0")
 
-    ######### REMOVE POINTS BELOW Z=0 #########
-    points = np.asarray(pcd.points)
-    pcd_sel = pcd.select_by_index(np.where(points[:, 2] < 0)[0])
+    inlier_cloud = pcd.select_by_index(inliers) #select the inliers
+    inlier_cloud.paint_uniform_color([1.0, 0, 0]) #set red for inlier
+    outlier_cloud = pcd.select_by_index(inliers, invert=True) #invert=True to select the outliers
+    outlier_cloud.paint_uniform_color([0, 0, 1.0]) #set blue for outlier
+    o3d.visualization.draw_geometries([inlier_cloud, outlier_cloud],
+                                    zoom=0.8,
+                                    front=[-0.4999, -0.1659, -0.8499],
+                                    lookat=[2.1813, 2.0619, 2.0999],
+                                    up=[0.1204, -0.9852, 0.1215])
+    ######### REMOVE POINTS BELOW Z=0 ######### 
+    # points = np.asarray(pcd.points)
+    # pcd_sel = pcd.select_by_index(np.where(points[:, 2] < 0)[0])
 
-    # visualize different point clouds
-    o3d.visualization.draw_geometries([pcd])
-    o3d.visualization.draw_geometries([pcd_sel])
+    # # visualize different point clouds
+    # o3d.visualization.draw_geometries([pcd])
+    # o3d.visualization.draw_geometries([pcd_sel])
