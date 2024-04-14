871da0c07a47312fab72a4104ba9f8eb1c2bf982
KocurMaros <kocur.maros@gmail.com>
2024-04-14 20:34:00 +0200

    visualise pcd

--- exam4.py
+++ exam4.py
@@ -1,18 +1,61 @@
+# import numpy as np
+# import open3d as o3d
+
+# if __name__ == "__main__":
+#     print("Testing IO for point cloud ...")
+#     pcd = o3d.io.read_point_cloud("office1.pcd", format='pts')
+#     print(pcd)
+#     o3d.io.write_point_cloud("copy_of_fragment.ply", pcd)
+#     # o3d.io.write_point_cloud("pcd/segmentation/mOSD/learn/learn0.pcd", pcd)
+
+#     print("Load a ply point cloud, print it, and render it")
+#     pcd1 = o3d.io.read_point_cloud("copy_of_fragment.ply")
+#     print(pcd1)
+#     print(np.asarray(pcd1.points))
+#     o3d.visualization.draw_geometries([pcd1])
+
+    # print("Downsample the point cloud with a voxel of 0.05")
+    # downpcd = pcd1.voxel_down_sample(voxel_size=0.05)
+    # o3d.visualization.draw_geometries([downpcd])
+
+    # print("Recompute the normal of the downsampled point cloud")
+    # downpcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(
+    #     radius=0.1, max_nn=30))
+    # o3d.visualization.draw_geometries([downpcd])
+
+    # print("Print a normal vector of the 0th point")
+    # print(downpcd.normals[0])
+    # print("Print the normal vectors of the first 10 points")
+    # print(np.asarray(downpcd.normals)[:10, :])
+    # print("")
+# import open3d as o3d
+
+# cloud = o3d.io.read_point_cloud('pcd/segmentation/mOSD/learn/learn1.pcd')
+# print(np.asarray(cloud.points))
+# print(np.asarray(cloud.colors))
+# print(np.asarray(cloud.normals))
+# o3d.visualization.draw_geometries([cloud])
 import open3d as o3d
-from pathlib import Path
+import numpy as np
 
-pcs_path = Path("point_clouds")
-pcd = o3d.io.read_point_cloud(str(pcs_path.joinpath("01.pcd")))
+# Read the point cloud
+pcd = o3d.io.read_point_cloud('office1.pcd')
 
-vis = o3d.visualization.Visualizer()
-vis.create_window()
-vis.add_geometry(pcd)
+# Convert points to numpy array
+points = np.asarray(pcd.points)
 
-for p in pcs_path.iterdir():
-    pcd = o3d.io.read_point_cloud(str(p))
-    
-    vis.update_geometry(pcd)
-    vis.poll_events()
-    vis.update_renderer()
+# Create a mask of boolean values representing whether each point is a real number
+mask = ~np.isnan(points).any(axis=1)
 
-vis.destroy_window()
+# Use the mask to filter the points
+real_points = points[mask]
+
+# Translate and scale the points
+real_points = real_points - np.mean(real_points, axis=0)
+real_points = real_points / np.std(real_points, axis=0)
+
+# Update the point cloud with the new points
+pcd.points = o3d.utility.Vector3dVector(real_points)
+
+# Visualize the point cloud
+o3d.visualization.draw_geometries([pcd])
